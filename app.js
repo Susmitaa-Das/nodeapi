fd8e90fbf3bf5f7c4ce010dfc582b11ba8d16db1
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-07-16 01:08:14 +0530

    Updated app js file

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 const app = express()
+//Using Meadelwears
+app.use(express.json())
 
 mongoose
   .connect('mongodb://127.0.0.1:27017', {
@@ -10,13 +12,27 @@ mongoose
   .then(() => console.log('Database Connected'))
   .catch((e) => console.log(e))
 
+const schema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+})
+
+const User = mongoose.model('User', schema)
+
 app.get('/', (req, res) => {
   res.send('Nice working')
 })
-app.get('/users/all', (req, res) => {
-  res.json({
+app.post('/users/new', async (req, res) => {
+    const {name,email,password} = req.body
+   await User.create({
+    name,
+    email,
+    password,
+  })
+  res.status(201).json({
     success: true,
-    users: [],
+    message:'Registered successfully',
   })
 })
 
